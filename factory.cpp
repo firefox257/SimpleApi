c27e4c6b6bbcca46897a26bd2ad20ea7739e54dc
firefox257 <49958313+firefox257@users.noreply.github.com>
2021-04-05 07:04:12 -0700

    Create factory.cpp

---
+++ factory.cpp
@@ -0,0 +1,53 @@
+
+#ifndef FACTORY_CPP
+#define FACTORY_CPP
+
+#include <functional>
+
+
+
+template <class N>
+class factory
+{
+	static function<N*()> _get;
+	
+	static bool _isstatic;
+	
+	N * _n;
+	public:
+	
+	static void set(function<N*()> func, bool isstatic)
+	{
+		_get = func;
+		_isstatic = isstatic;
+	}
+	
+	inline static N* get()
+	{
+		return _get();
+	}
+	
+	factory()
+	{
+		_n = _get();
+	}
+	~factory()
+	{
+		
+		if(!_isstatic)
+		{
+			delete(_n);
+		}
+	}
+	N * operator ->()
+	{
+		return _n;
+	}
+	
+};
+template<class N>
+function<N*()> factory<N>::_get;
+template<class N>
+bool factory<N>::_isstatic;
+
+#endif
