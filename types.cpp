351d26a36a7f04605af3969f87ceecb5416b8696
firefox257 <49958313+firefox257@users.noreply.github.com>
2021-04-12 15:01:51 -0600

    Create types.cpp

---
+++ types.cpp
@@ -0,0 +1,17 @@
+#ifndef TYPES_CPP
+#define TYPES_CPP
+
+typedef bool mbool;
+typedef unsigned char muchar;
+typedef char mchar;
+typedef unsigned short mushort;
+typedef short mshort;
+typedef unsigned int muint;
+typedef int mint;
+typedef unsigned long long mulong;
+typedef long long mlong;
+typedef float mfloat;
+typedef double mdouble;
+typedef long double mldouble;
+
+#endif
